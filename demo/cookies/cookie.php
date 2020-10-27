6031f2566f606b206bac7dcc0f3855744085cdfe
AlexZandr87Russia <alex@biofam.ru>
2020-10-27 14:43:01 +0500

    Work_10_27_1

--- demo/cookies/cookie.php
+++ demo/cookies/cookie.php
@@ -1,4 +1,8 @@
 <?php
-	setcookie("userName", 'John');
-
-	echo $_COOKIE['userName'];
+function zero_one($n){
+    for ($i = 0; $i <= $n; $i++){
+        $affordable[] = ($i % 2 == 0) ? 0 : 1;
+    }
+    print_r($affordable);
+}
+zero_one(10);
